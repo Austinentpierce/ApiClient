9d81b79dffa2cea90aba06ea3b5858d1719a84fa
Austin Pierce <Austinentpierce@gmail.com>
2021-08-17 23:00:34 -0400

    going to change these into Json hints for the program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static async Task Main(string[] args)
+        {
+            var client = newHttpClient();
+            var currentDate = DateTime.Now;
+            var responseBodyAsStreamTen = await client.GetStreamAsync("https://official-joke-api.appspot.com/jokes/ten");
+            var items = await JsonDeserializeAsync<List<Item>>(responseBodyAsStreamTen);
+
+
+        }
+
+
     }
-  }
 }
