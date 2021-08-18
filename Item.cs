93172c259aa54a47acbd16326f3cac568dc68650
Austin Pierce <Austinentpierce@gmail.com>
2021-08-17 21:55:23 -0400

    added item class

---
+++ Item.cs
@@ -0,0 +1,12 @@
+namespace ApiClient
+{
+    public class Item
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public bool complete { get; set; }
+        public string completed_at { get; set; }
+        public string updated_at { get; set; }
+
+    }
+}
