14d3e4b5f0a44c37439846d2a465a1171d3120e8
Austin Pierce <Austinentpierce@gmail.com>
2021-08-17 23:00:08 -0400

    added new using statements and new variables

--- Item.cs
+++ Item.cs
@@ -4,10 +4,9 @@ namespace ApiClient
     public class Item
     {
         public int id { get; set; }
-        public string text { get; set; }
-        public bool complete { get; set; }
-        public DateTime completed_at { get; set; }
-        public DateTime updated_at { get; set; }
+        public string type { get; set; }
+        public string setup { get; set; }
+        public string punchline { get; set; }
 
     }
 }
