e0c3fa22cec50fef36483fcf4118c84355a2017e
Austin Pierce <Austinentpierce@gmail.com>
2021-08-20 09:38:34 -0400

    got data back from api

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
 using System.Threading.Tasks;
+
 
 namespace ApiClient
 {
@@ -8,11 +11,11 @@ namespace ApiClient
     {
         static async Task Main(string[] args)
         {
-            var client = newHttpClient();
-            var currentDate = DateTime.Now;
-            var responseBodyAsStreamTen = await client.GetStreamAsync("https://official-joke-api.appspot.com/jokes/ten");
-            var items = await JsonDeserializeAsync<List<Item>>(responseBodyAsStreamTen);
-
+            var client = new HttpClient();
+            //var currentDate = DateTime.Now;
+            var responseBodyAsString = await client.GetStringAsync("https://official-joke-api.appspot.com/jokes/ten");
+            //var items = await JsonDeserializeAsync<List<Item>>(responseBodyAsStreamTen);
+            Console.WriteLine(responseBodyAsString);
 
         }
 
