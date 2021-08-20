50ee8961a12b2152741845efa5554650fa83023e
Austin Pierce <Austinentpierce@gmail.com>
2021-08-20 08:43:01 -0400

    updated item class going to finish program.cs

--- Item.cs
+++ Item.cs
@@ -1,11 +1,20 @@
 using System;
+using System.Text.Json.Serialization;
+
 namespace ApiClient
 {
     public class Item
     {
+        [JsonPropertyName("id")]
         public int id { get; set; }
+
+        [JsonPropertyName("type")]
         public string type { get; set; }
+
+        [JsonPropertyName("setup")]
         public string setup { get; set; }
+
+        [JsonPropertyName("punchline")]
         public string punchline { get; set; }
 
     }
